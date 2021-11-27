9c59814be6e78c4d35ca4986fc26758b0c1f2c48
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-11-27 12:37:27 +0100

    add first version of input fields

--- src/features/input/InputFields.vue
+++ src/features/input/InputFields.vue
@@ -1,4 +1,14 @@
+<template>
+Job Name: <input v-model="job_name" placeholder="Job Name"><br>
+Resolution: <input v-model="resolution" placeholder="Resolution"><br>
+Cloud Cover: <input v-model="cloud_cover" placeholder="Cloud Cover"><br>
+Start Timestamp: <input v-model="start_timestamp" placeholder="Start timestamp"><br>
+End Timestamp: <input v-model="end_timestamp" placeholder="End timestamp"><br>
+Response: <input v-model="response" placeholder="response"><br>
+Samples: <input v-model="samples" placeholder="samples"><br>
+Area of Interest: <input v-model="aoi" placeholder="Area of Interest"><br>
 
+</template>
 
 <script>
 export default {
