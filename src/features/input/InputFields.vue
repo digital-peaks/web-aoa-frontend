b3ec9bb09a2f797dae0c1e941773ff6da29d60f2
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-11-27 12:45:02 +0100

    add data upload fields for AOI and Sample

--- src/features/input/InputFields.vue
+++ src/features/input/InputFields.vue
@@ -1,13 +1,41 @@
 <template>
-Job Name: <input v-model="job_name" placeholder="Job Name"><br>
-Resolution: <input v-model="resolution" placeholder="Resolution"><br>
-Cloud Cover: <input v-model="cloud_cover" placeholder="Cloud Cover"><br>
-Start Timestamp: <input v-model="start_timestamp" placeholder="Start timestamp"><br>
-End Timestamp: <input v-model="end_timestamp" placeholder="End timestamp"><br>
-Response: <input v-model="response" placeholder="response"><br>
-Samples: <input v-model="samples" placeholder="samples"><br>
-Area of Interest: <input v-model="aoi" placeholder="Area of Interest"><br>
+  <!-- Textfields-->
+  Job Name: <input v-model="job_name" placeholder="Job Name" /><br />
+  Resolution: <input v-model="resolution" placeholder="Resolution" /><br />
+  Cloud Cover: <input v-model="cloud_cover" placeholder="Cloud Cover" /><br />
+  Start Timestamp:
+  <input v-model="start_timestamp" placeholder="Start timestamp" /><br />
+  End Timestamp:
+  <input v-model="end_timestamp" placeholder="End timestamp" /><br />
+  Response: <input v-model="response" placeholder="response" /><br />
+  Samples: <input v-model="samples" placeholder="samples" /><br />
+  Area of Interest: <input v-model="aoi" placeholder="Area of Interest" /><br />
 
+
+
+  <!-- File Upload Fields-->
+  <br /><br />
+  File Upload for GeoJSON Data: <br /><br />
+  <label>
+    Sample GeoJSON <br />
+    <input
+      type="file"
+      id="sample_GJSON_File"
+      ref="file"
+      v-on:change="handleFileUpload()"
+    />
+    <br />
+    <br />
+    Area of Interest GeoJSON <br />
+    <input
+      type="file"
+      id="AOI_GJSON_File"
+      ref="file"
+      v-on:change="handleFileUpload()"
+    />
+    <br />
+  </label>
+  <button v-on:click="submitFile()">Submit</button>
 </template>
 
 <script>
