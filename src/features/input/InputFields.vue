41581ca27b746fdccf976b48d1242e5f4166c33d
Noel Schnierer <noel.schnierer@gmail.com>
2021-11-28 21:06:16 +0100

    add first job form

--- src/features/input/InputFields.vue
+++ src/features/input/InputFields.vue
@@ -37,16 +37,13 @@
   <button v-on:click="calculateAOI()">Calculate Area of Interest</button>
 
   <!--Leaflet -->
-<l-map style="height: 300px" :zoom="zoom" :center="center">
+  <l-map style="height: 300px" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker :lat-lng="markerLatLng"></l-marker>
   </l-map>
-  
 </template>
 
 <script>
-
-
 export default {
   name: "InputFields",
   props: {
