82474b150bdd861172e42ef0516aa38fe88b035c
Josefina Balzer <jolaubal@gmail.com>
2021-12-08 17:37:11 +0100

    asking for help...

--- src/features/output/OutputView.vue
+++ src/features/output/OutputView.vue
@@ -8,12 +8,9 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-<<<<<<< Updated upstream
 import "leaflet/dist/leaflet.css";
 import "geotiff"; // Only EPSG:4326 is supported!!!
-=======
-//import "geotiff/src/geotiff.js"; // Only EPSG:4326 is supported!!!
->>>>>>> Stashed changes
+
 //import "plotty/dist/plotty.js";
 import "georaster";
 //import "georaster-layer-for-leaflet";
@@ -21,8 +18,6 @@ import "leaflet-geotiff-2/dist/leaflet-geotiff.js";
 import "leaflet-geotiff-2/dist/leaflet-geotiff-rgb";
 import "leaflet-geotiff-2/dist/leaflet-geotiff-vector-arrows";
 import "leaflet-geotiff-2/dist/leaflet-geotiff-plotty";
-
-import test from "geotiffs_test/aoa_aoa.tif";
 
 import parseGeoraster from "georaster";
 import GeoRasterLayer from "georaster-layer-for-leaflet";
@@ -32,23 +27,11 @@ export default {
   data: () => ({
     map: null,
     tileLayer: null,
-    geoTiffUrl: "../../../geotiff_test/aoa_aoa.tif",
+    geoTiffUrl: "../geotiff_test/aoa_aoa.tif",
     options: null,
-    geotiffLayer: null,
+    //geotiffLayer: null,
   }),
-  chainWebpack: (config) => {
-    config.module
-      .rule("raw")
-      .test(/\.tif$/)
-      .use("raw-loader")
-      .loader("raw-loader")
-      .end();
-  },
   methods: {
-    parseGeoraster,
-    getGeoTiff() {
-      console.log({ test });
-    },
     initMap: function () {
       this.map = L.map("map-container").setView([51.966, 7.633], 10);
       this.tileLayer = L.tileLayer(
@@ -60,29 +43,11 @@ export default {
       ).addTo(this.map);
     },
     testFunction2: async function () {
-      var txt = "";
-      var xmlhttp = new XMLHttpRequest();
-
-      xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
-          txt = xmlhttp.responseText;
-        }
-      };
-      xmlhttp.open("GET", "\test.txt", true);
-      xmlhttp.send();
-
-      console.log(txt);
-
-      /*
-
       const response = await fetch(this.geoTiffUrl);
-      const blobber = response.blob();
-      console.log(blobber);
-      debugger;
       const arrayBuffer = await response.arrayBuffer();
       console.log(arrayBuffer);
-      const parsedGeoraster = await parseGeoraster(arrayBuffer);
-      console.log(parsedGeoraster);*/
+      const parsed = await parseGeoraster(arrayBuffer); // Hier schlägt es fehl. Das was als arrayBuffer gespeichert ist, kann nicht geparsed werden... Ich dachte allerdings, dass auf diesem Wege das gleich erzielt würde, wie beim aktiven Upload (siehe testFunction() und Ausgabe auf der Console von arrayBuffer). Verstehst du, warum das nicht klappt?
+      console.log(parsed);
       /*
         .then((response) => response.arrayBuffer())
         .then((arrayBuffer) => {
@@ -91,7 +56,6 @@ export default {
           });
         });
       console.log("res: " + res);
-
       fetch(this.geoTiffUrl)
         .then((response) => response.arrayBuffer())
         .then((arrayBuffer) => {
@@ -101,93 +65,45 @@ export default {
             console.log("georaster:", georaster);
           });
         });
-
+      const response = await fetch(this.geoTiffUrl);
       const response = await fetch(this.geoTiffUrl);
       console.log(response);
       const arrayBuffer = await response.arrayBuffer();
-      console.log("buffer:", arrayBuffer);
-
-      const tiff = await fromArrayBuffer(arrayBuffer);
-      console.log(tiff);*/
-      /*const georaster = await parseGeoraster(arrayBuffer);
-      console.log("georaster:", georaster);
-
-      let layer = new GeoRasterLayer({
-        georaster: georaster,
-        opacity: 0.7,
-        resolution: 256,
-      });
-      console.log("layer:", layer);
-      layer.addTo(this.map);
-
-      this.map.fitBounds(layer.getBounds());*/
+      console.log("buffer:", arrayBuffer);*/
     },
-<<<<<<< Updated upstream
     testFunction: function (event) {
       // debugger;
       const file = event.target.files[0];
       console.log("file:", file);
 
-      let reader = new FileReader();
+      const reader = new FileReader();
       reader.readAsArrayBuffer(file);
+      console.log(reader);
       reader.onloadend = async () => {
         const arrayBuffer = reader.result;
         console.log("buffer: ", arrayBuffer);
         const georaster = await parseGeoraster(arrayBuffer);
         console.log("georaster:", georaster);
 
-        let layer = new GeoRasterLayer({
+        /*
+              GeoRasterLayer is an extension of GridLayer,
+              which means can use GridLayer options like opacity.
+
+              Just make sure to include the georaster option!
+
+              http://leafletjs.com/reference-1.2.0.html#gridlayer
+            */
+        var layer = new GeoRasterLayer({
           georaster: georaster,
           opacity: 0.7,
           resolution: 256,
-=======
-    testFunction: function () {
-      document
-        .getElementById("fileInput")
-        .addEventListener("change", function (event) {
-          var file = event.target.files[0];
-          console.log("file:", file);
-
-          this.geotiffLayer = new window.L.FeatureGroup().addTo(this.map); ////
-
-          var reader = new FileReader();
-          console.log(reader);
-          reader.readAsArrayBuffer(file);
-          reader.onloadend = function () {
-            var arrayBuffer = reader.result;
-            parseGeoraster(arrayBuffer).then((georaster) => {
-              console.log("georaster:", georaster);
-              /*
-                GeoRasterLayer is an extension of GridLayer,
-                which means can use GridLayer options like opacity.
-
-                Just make sure to include the georaster option!
-
-                http://leafletjs.com/reference-1.2.0.html#gridlayer
-            */
-              var layer = new GeoRasterLayer({
-                georaster: georaster,
-                opacity: 0.7,
-                resolution: 256,
-              });
-              console.log("layer:", layer);
-              console.log("geotifflayer before: " + this.geotiffLayer);
-              this.geotiffLayer.addLayer(this.geotiffLayer);
-              console.log("geotifflayer after: " + this.geotiffLayer);
-              //layer.addTo(this.map);
-
-              this.map.fitBounds(layer.getBounds());
-              document.getElementById("overlay").style.display = "none";
-            });
-          };
->>>>>>> Stashed changes
         });
         console.log("layer:", layer);
         layer.addTo(this.map);
-
         this.map.fitBounds(layer.getBounds());
-        // document.getElementById("overlay").style.display = "none";
+        //document.getElementById("overlay").style.display = "none";
       };
+      // document.getElementById("overlay").style.display = "none";
     },
   },
   mounted() {
