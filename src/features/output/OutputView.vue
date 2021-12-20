791fd6e32bb28a1e8c2a637b0784cf1f7ffb3178
Josefina Balzer <jolaubal@gmail.com>
2021-12-20 10:00:23 +0100

    comments added

--- src/features/output/OutputView.vue
+++ src/features/output/OutputView.vue
@@ -242,28 +242,32 @@ export default {
   data: () => ({
     map: null,
     tileLayer: null,
-    // In most cases it's just "/aoa_example.tif"
-    // See: https://cli.vuejs.org/config/#baseurl
-    // Here you have to link the .tif-folder given from the r-script
+    // Everything needed to visualize the aoi.geojson.
     aoiJson: `${process.env.BASE_URL}geotiffs_test/aoi.geojson`,
     aoiLayer: null,
     aoiTransparency: 50,
     aoiLineThickness: 1,
+    // Everything needed to visualize the aoi_di.tif.
     diUrl: `${process.env.BASE_URL}geotiffs_test/aoa_di.tif`,
     diLayer: null,
     diTransparency: 100,
+    // Everything needed to visualize the pred.tif.
     predUrl: `${process.env.BASE_URL}geotiffs_test/pred.tif`,
     predLayer: null,
     predTransparency: 100,
+    // Everything needed to visualize the aoi_aoa.tif.
     aoaUrl: `${process.env.BASE_URL}geotiffs_test/aoa_aoa.tif`,
     aoaLayer: null,
     aoaTransparency: 100,
+    // Everything needed to visualize the samplePolygons.geojson.
     samplePolygonsJson: `${process.env.BASE_URL}geotiffs_test/samplePolygons.geojson`,
     samplePolygonsLayer: null,
     samplePolygonsTransparency: 50,
     samplePolygonsLineThickness: 1,
+    // Everything needed to visualize the suggestion.geojson.
     suggestionJson: `${process.env.BASE_URL}geotiffs_test/suggestion.geojson`,
     suggestionLayer: null,
+    // Causes the percentage scale of the slider component.
     sliderPercentage: "{value} %",
   }),
   components: {
@@ -273,7 +277,7 @@ export default {
   },
   methods: {
     /**
-     *
+     * This function initializes the leaflet map with an osm tile layer and focused on Münster.
      */
     initMap: function () {
       this.map = L.map("map-container").setView([51.966, 7.633], 10);
@@ -285,6 +289,11 @@ export default {
         }
       ).addTo(this.map);
     },
+    /**
+     * this function triggers the downlad process of the result of the calculations.
+     * @param {string} urlLink -  Contains the internal URL to the file.
+     * @param {string} label - Contains the label the downloaded file should get.
+     */
     downloadItem: async function (urlLink, label) {
       const url = `${process.env.BASE_URL}` + urlLink;
       let response = await axios.get(url, { responseType: "blob" });
@@ -297,12 +306,21 @@ export default {
       link.click();
       URL.revokeObjectURL(link.href);
     },
+    /**
+     * This function checks whether a requested layer exists or not
+     * @param {GeoRasterLayer / GeoJson} layer - This parameter contains the requested layer.
+     * @return {Boolean} - It returns true, if the layer eists and false if not.
+     */
     checkLayerGetsFoundWithMessage: function (layer) {
       if (layer == null) {
         console.log("Dieser Layer existiert nicht");
         return false;
       } else return true;
     },
+    /**
+     * This function changes the opacity when triggered through a button.
+     * @param {string} layerId - The layerId must be given to change the opacity for the right layer.
+     */
     changeOpacity: function (layerId) {
       if (layerId == "aoi") {
         this.aoiLayer.setStyle({
@@ -320,6 +338,10 @@ export default {
         });
       }
     },
+    /**
+     * This function changes the line thickness of the polygons in a GeoJson layer.
+     * @param {string} layerId - The layerId must be given to change the line thickness for the right layer.
+     */
     changeLineThickness: function (layerId) {
       if (layerId == "aoi") {
         this.aoiLayer.setStyle({
@@ -331,13 +353,18 @@ export default {
         });
       }
     },
+    /**
+     * This function (de-)activates the layer when it gets triffered by the Show/Hide checkbox.
+     * @param {string} id - The id is needed to know which layer got triggered.
+     */
     switchLayer: function (id) {
       let tempLayer = null;
       let checked = document.getElementById(id).checked;
       if (!checked) checked = true;
-      // This line and the following one convert the value because vuetify uses switched values
+      // This line and the following one convert the value because vuetify uses switched values.
       else checked = false;
       if (checked) {
+        // In case the checkbox is after clicking it checked the layer gets activated.
         if (id == "aoi") {
           tempLayer = this.aoiLayer;
           if (!this.checkLayerGetsFoundWithMessage(tempLayer))
@@ -366,6 +393,7 @@ export default {
         tempLayer.addTo(this.map);
         this.map.fitBounds(tempLayer.getBounds());
       } else if (!checked) {
+        // In case the checkbox is after clicking it unchecked the layer gets deactivated.
         if (id == "aoi") {
           this.map.removeLayer(this.aoiLayer);
         } else if (id == "di") {
@@ -381,8 +409,11 @@ export default {
         }
       }
     },
+    /**
+     * This function focuses the map to the layer where the button gets triggered. It's comparable to the "zoom to layer" function known from common GIS.
+     * @param {string} layerId - The layer id is needed to kn ow which layer got triggered.
+     */
     zoomToLayer: function (layerId) {
-      // STOPPED
       let tempLayer = null;
       if (layerId == "aoi") tempLayer = this.aoiLayer;
       else if (layerId == "di") tempLayer = this.diLayer;
@@ -394,6 +425,9 @@ export default {
       else return;
       this.map.fitBounds(tempLayer.getBounds());
     },
+    /**
+     * This function builds layers for all .geojson files.
+     */
     showGeoJson: async function () {
       const responseAoi = await fetch(this.aoiJson);
       const responseSamplePolygons = await fetch(this.samplePolygonsJson);
@@ -404,11 +438,12 @@ export default {
       const suggestion = await responseSuggestion.json();
 
       this.aoiLayer = L.geoJson().addData(aoi);
-      this.samplePolygonsLayer = L.geoJson(samplePolygons, {
-        style: { fillOpacity: this.samplePolygonsTransparency / 100 },
-      });
+      this.samplePolygonsLayer = L.geoJson(samplePolygons);
       this.suggestionLayer = L.geoJson(suggestion);
     },
+    /**
+     * This function builds layers for all .tif files.
+     */
     showTif1Band: async function () {
       const responseDi = await fetch(this.diUrl);
       const responseAoa = await fetch(this.aoaUrl);
