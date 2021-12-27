f48f02ff5d199060e71d3bc747bff7ce71cd8196
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-12-27 21:40:56 +0100

    add basic stuff (adress, github-link)

--- src/features/impressum/ImpressumView.vue
+++ src/features/impressum/ImpressumView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container text-start">
     <h1>Impressum</h1>
-    <p>To be defined...</p>
+    <p>Digital Peaks<br>
+      Institut für Geoinformatik<br>
+      Heisenbergstraße 2<br>
+      48149 Münster<br>
+    </p>
+    <a href="https://github.com/digital-peaks"> Visit us on Github </a>
+   
+      
+    
   </div>
 </template>
