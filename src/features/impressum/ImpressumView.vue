9d6401c4b99cd751a41015bd53d0ec273f0b8f86
Josefina Balzer <jolaubal@gmail.com>
2021-12-29 12:32:10 +0100

    added some pt and h2

--- src/features/impressum/ImpressumView.vue
+++ src/features/impressum/ImpressumView.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="container text-start">
+  <div class="container pt-10">
     <h1>Impressum</h1>
-    <p>Digital Peaks<br>
-      Institut für Geoinformatik<br>
-      Heisenbergstraße 2<br>
-      48149 Münster<br>
+    <h2>Angaben gemäß §5 TMG</h2>
+    <p>
+      Digital Peaks<br />
+      Institut für Geoinformatik<br />
+      Heisenbergstraße 2<br />
+      48149 Münster<br />
     </p>
     <a href="https://github.com/digital-peaks"> Visit us on Github </a>
-   
-      
-    
   </div>
 </template>
