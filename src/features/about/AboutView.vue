91d68654fdb2e89baad00a22fe6d55805b142a2a
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2022-01-12 16:18:06 +0100

    add all in one page

--- src/features/about/AboutView.vue
+++ src/features/about/AboutView.vue
@@ -2,58 +2,52 @@
   <div class="container text-start">
     <h1>About</h1>
     <v-row justify="center">
-      <v-expansion-panels accordion>
-        <v-expansion-panel>
-          <v-expansion-panel-header><h5>What kind of website is this?</h5></v-expansion-panel-header
-          >
-          <v-expansion-panel-content>
-            This web application aims to implement the AOA method according to
-            Meyer & Pebesma (2021). For this purpose the LU/LC classification
-            was used.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
+      <h5>What kind of website is this?</h5>
+      <p>
+        This web application aims to implement the AOA method according to Meyer
+        & Pebesma (2021). For this purpose the LU/LC classification was used.
+      </p>
+      <br />
+      <br />
 
-        <v-expansion-panel>
-          <v-expansion-panel-header> <h5>Project reference</h5></v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Machine learning has become a popular method to classify remote
-            sensing data. method for the classification of remote sensing data.
-            Possible classifications can be found in the context of LULC. The
-            possibility of using machine learning to capture non-linear
-            correlations and thus to enable complex classifications is certainly
-            one of the main reasons for this. for this. Usually, such models are
-            trained for spaces in which the actual values of the target
-            variables are known. In addition, however, methods have to be found
-            methods must be found and provided, which allow the user to estimate
-            how good an already how well an already trained model can be applied
-            to a new space, i.e. a space where the target variables are a space,
-            in which the target variables are to be determined first. The Area
-            of Applicability method, as proposed by Meyer and Pebesma (Meyer and
-            Pebesma 2021), solves exactly this problem and allows the user to
-            make a spatial estimate of how reliable the target variables are.
-            estimate how reliable a machine classification algorithm is likely
-            to work in a new application area. is able to perform in a new
-            application domain. Since currently knowledge of the language R
-            language is required for the use of the AOA method, an
-            infrastructure shall be created which allows a much larger spectrum
-            of users to use the AOA method for their own purposes. purposes.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
+      <h5>Project reference</h5>
+      <p>
+        Machine learning has become a popular method to classify remote sensing
+        data. method for the classification of remote sensing data. Possible
+        classifications can be found in the context of LULC. The possibility of
+        using machine learning to capture non-linear correlations and thus to
+        enable complex classifications is certainly one of the main reasons for
+        this. for this. Usually, such models are trained for spaces in which the
+        actual values of the target variables are known.
+        <br />
+        In addition, however, methods have to be found methods must be found and
+        provided, which allow the user to estimate how good an already how well
+        an already trained model can be applied to a new space, i.e. a space
+        where the target variables are a space, in which the target variables
+        are to be determined first. The Area of Applicability method, as
+        proposed by Meyer and Pebesma (Meyer and Pebesma 2021), solves exactly
+        this problem and allows the user to make a spatial estimate of how
+        reliable the target variables are. estimate how reliable a machine
+        classification algorithm is likely to work in a new application area. is
+        able to perform in a new application domain.
+        <br />
+        Since currently knowledge of the language R language is required for the
+        use of the AOA method, an infrastructure shall be created which allows a
+        much larger spectrum of users to use the AOA method for their own
+        purposes. purposes.
+      </p>
 
-        <v-expansion-panel>
-          <v-expansion-panel-header> <h5>Who are we?</h5></v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Our company is a five-person project team, which is specialized in
-            the development of modern modern web applications with a focus on
-            geospatial data processing. The business partner is WWU Münster. The
-            order was submitted by the department 14 - Geosciences.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <h5>Who are we?</h5>
+      <p>
+        Our company is a five-person project team, which is specialized in the
+        development of modern modern web applications with a focus on geospatial
+        data processing. The business partner is WWU Münster. The order was
+        submitted by the department 14 - Geosciences.
+      </p>
     </v-row>
 
-    <br>
-    
+    <br />
+
     <v-btn
       elevation="0"
       color="white"
@@ -62,6 +56,5 @@
     >
       <v-icon>mdi-github</v-icon> Github
     </v-btn>
-    
-      </div>
+  </div>
 </template>
