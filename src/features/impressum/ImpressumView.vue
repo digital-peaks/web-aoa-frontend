091770166af4e0ce37123d8343a2b05d03790df0
Josefina Balzer <jolaubal@gmail.com>
2022-01-23 19:46:04 +0100

    gaps added/removed

--- src/features/impressum/ImpressumView.vue
+++ src/features/impressum/ImpressumView.vue
@@ -2,7 +2,7 @@
   <div class="container pt-10">
     <h1>Impressum</h1>
     <v-row justify="center">
-      <h2>Angaben gemäß §5 TMG</h2>
+      <h2 class="pt-4">Angaben gemäß §5 TMG</h2>
       <p>
         Digital Peaks<br />
         Institut für Geoinformatik<br />
