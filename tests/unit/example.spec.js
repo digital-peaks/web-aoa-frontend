be686bc911b77f9193f8728bdc4532c7d7fc491c
Josefina Balzer <jolaubal@gmail.com>
2022-01-27 18:56:29 +0100

    doesnt work to check on the components

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,8 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 //import InputView from "../../src/features/input/InputView.vue";
 import AboutView from "../../src/features/about/AboutView.vue";
 import ImpressumView from "../../src/features/impressum/ImpressumView.vue";
 import LoginView from "../../src/features/login/LoginView.vue";
+import LogoIcon from "../../src/components/LogoIcon.vue";
 
 describe("AboutView", () => {
   it("renders the input page", () => {
@@ -22,5 +23,17 @@ describe("LoginView", () => {
   it("renders the login page", () => {
     const wrapper = shallowMount(LoginView);
     expect(wrapper.contains("div")).toBe(true);
+  });
+});
+
+describe("LogoIcon", () => {
+  it("checks Logo icon component", () => {
+    const wrapper = mount(LogoIcon, {
+      propsData: {
+        width: "200",
+        height: "200",
+      },
+    });
+    expect(wrapper.html().toMatchSnapshot());
   });
 });
