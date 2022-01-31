7e129faca03948967adc819f131824c319886ef3
Josefina Balzer <jolaubal@gmail.com>
2022-01-31 18:34:05 +0100

    removed error

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
-const esModules = ["leaflet"];
-
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  /*transform: {
-    "^.+\\.js$": "babel-jest",
-    ".*\\.(vue)$": "vue-jest",
-  },
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],*/
 };
